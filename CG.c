d41776889261daa2de575d649c97ea6bc8100dd9
ChrisPB93 <prindsbilberg@wirelessprvnat-172-17-69-157.near.illinois.edu>
2015-11-30 17:38:26 -0600

    CG.c CG.h

--- CG.c
+++ CG.c
@@ -5,7 +5,7 @@
 #include "testcase.h"
 
 
-void luDecom(int n,  double A[][n], double f[n], double x[n]) {
+void conjGrad(int n,  double A[][n], double f[n], double x[n]) {
 	int k = 0;
 	double tolerance = 1e-4;
 	int maxIterations = 1000;
@@ -22,14 +22,13 @@ void luDecom(int n,  double A[][n], double f[n], double x[n]) {
 			p = r;
 		} else {
 			beta = dotProduct(n, r, r) / dotProduct(n, rPrev, rPrev);
-			p = r + beta * p;
+			vectorAdd(n, beta, r, p, p);
 		}
-		s = A . p;
+		matrixVectorMult(n, A, p, s);
 		alpha = dotProduct(n, r, r) / dotProduct(n, p, s);
-		x = x + alpha * p;
-
+		vectorAdd(n, alpha, x, p, x);	
 		rPrev = r;
-		r = r - alpha * s;
+		vectorAdd(n, -alpha, r, s, r);
 	}
 }
 
@@ -40,3 +39,33 @@ double dotProduct(int n,int a[n], int b[n]) {
 	}
 	return res;
 }
+
+// Functions
+
+void matrixVectorMult(int n, double M[n][n], double V[n], double R[n])
+{
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			R[i] += M[i][j] * V[i];
+		}
+	}
+}
+
+void vectorAdd(int n, double S, double V1[n], double V2[n], double R[n])
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		R[i] = V1[i] + S * V2[i];
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
